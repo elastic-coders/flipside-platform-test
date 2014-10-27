c061d8baaf1d15ff56e88881667d3843f6889d12
Marco Paolini <marco@credra.com>
2014-10-28 00:12:14 +0100

    Fixes and enhachements to salt bootstrapping

--- Vagrantfile
+++ Vagrantfile
@@ -13,7 +13,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.synced_folder "salt/pillar/", "/srv/pillar/"
 
 
-  config.vm.provision "shell", path: "flipside_platform/provision.py"
+  config.vm.provision "shell", path: "flipside_platform/provision.py", args: ["--salt-version", "git v2014.7.0rc6"]
 
   config.vm.provider "virtualbox" do |vb|
     vb.customize ["modifyvm", :id, "--memory", "1024"]
--- flipside_platform/provision.py
+++ flipside_platform/provision.py
@@ -5,9 +5,10 @@ import json
 import time
 import urllib.request
 import tempfile
+import os
 
 
-def install_salt(standalone):
+def install_salt(standalone, version='stable'):
     '''Install Salt on the local machine'''
     resp = urllib.request.urlopen('https://bootstrap.saltstack.com')
     assert resp.status == 200
@@ -15,15 +16,22 @@ def install_salt(standalone):
         f.write(resp.read())
         f.flush()
         subprocess.check_call(
-            'sh {script} {opts}'.format(
+            'sh {script} {opts} -n -p python-git {version}'.format(
                 script=f.name,
-                opts='-M -N' if standalone else '-i local -A 127.0.0.1'
+                opts='-M -N -X' if standalone else '-i local -A 127.0.0.1',
+                version=version
             ).split()
         )
 
 
 def setup_salt(standalone):
+    # XXX Maybe we could simply use salt to... bootstrap salt
+    if not os.path.exists('/etc/salt/master.d'):
+        os.mkdir('/etc/salt/master.d')
+    with open('/etc/salt/master.d/flipside_gitfs', 'w+') as f:
+        f.write('fileserver_backend:\n  - roots\n  - gitfs\n\ngitfs_remotes:\n  - https://github.com/saltstack-formulas/nginx-formula.git')
     if standalone:
+        # XXX use minion.d instead
         with open('/etc/salt/minion', 'w+') as f:
             f.write('file_client:\n  local\n')
     else:
@@ -50,7 +58,7 @@ def highstate_salt(standalone):
     subprocess.check_call('sudo salt-call state.highstate'.split())
 
 
-def main(standalone=True, highstate=True):
+def main(standalone=True, highstate=True, salt_version=None):
     install_salt(standalone)
     setup_salt(standalone)
     test_salt(standalone)
@@ -63,5 +71,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-standalone', action='store_true')
     parser.add_argument('--no-highstate', action='store_true')
+    parser.add_argument('--salt-version', default='stable')
     args = parser.parse_args()
-    main(standalone=not args.no_standalone, highstate=not args.no_highstate)
+    main(standalone=not args.no_standalone, highstate=not args.no_highstate,
+         salt_version=args.salt_version)
